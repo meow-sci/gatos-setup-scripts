720867c48a8e22eff15a05d4f688f0b24bef628c
Alex Sherwin <alex-sherwin@users.noreply.github.com>
2026-06-13 21:01:12 -0400

    initial commit (untested), easiest way to test is push/pull

---
+++ alex/initial-shell-setup.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+
+set -e
+
+echo "Changing default shell to zsh for root user"
+chsh -s /bin/zsh root
+
+pkgs_to_install="starship"
+
+echo "Installing packages: ${pkgs_to_install}"
+apk update
+apk add ${pkgs_to_install}
+
+script_dir=$(dirname "$0")
+mkdir -p ~/.config
+
+echo "Setting up starship"
+echo 'eval "$(starship init zsh)"' >> ~/.zshrc
+cp "${script_dir}/starship.toml" ~/.config/starship.toml
+
+echo "Installing and setting up atuin"
+curl --proto '=https' --tlsv1.2 -LsSf https://setup.atuin.sh | sh -s -- --non-interactive
+cp "${script_dir}/atuin._config.toml" ~/.config/atuin/config.toml
